a17b71c3d0a8366cade50d3b3db9eb77cb53e4ff
alisidd1 <alisiddiqui@gmail.com>
2024-01-15 12:11:59 -0500

    Initial Commit

---
+++ text-expandable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  text-expandable
+//
+//  Created by Ali Siddiqui on 1/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
